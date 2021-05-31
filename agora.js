21e27087711925c1e114b9d8ac19f70eaffe0bf5
Magdalena Ramirez <mramir97@ucsc.edu>
2021-05-31 10:46:10 -0700

    updated files

--- agora.js
+++ agora.js
@@ -4,8 +4,8 @@ let handlefail = function (err) {
 
 let appId = "e55d9ba6f7b9402a87a301fc30fd2213";
 let globalStream;
-var isAudioMuted=false;
-var isVideoMuted=false;
+var isAudioMuted = false;
+var isVideoMuted = false;
 
 let client = AgoraRTC.createClient({
     mode: "live",
@@ -19,7 +19,7 @@ function removeMyVideoStream() {
     globalStream.stop();
 }
 
-function removeVideoStream(evt) { //video not you removes theirs
+function removeVideoStream(evt) {
     let stream = evt.stream;
     stream.stop();
     let remDiv = document.getElementById(stream.getId())
@@ -29,56 +29,57 @@ function removeVideoStream(evt) { //video not you removes theirs
 function addVideoStream(streamId) {
     console.log()
     let remoteContainer = document.getElementById("remoteStream");
-    let streamDiv = document.createElement("div"); //create div inside romote stream div
+    let streamDiv = document.createElement("div");
     streamDiv.id = streamId;
-    // streamDiv.style.transform = "rotateY(180deg)"; 
-    streamDiv.style.height = "250px" //same height of remote stream
+    // streamDiv.style.transform = "rotateY(180deg)";
+    streamDiv.style.height = "250px"
     remoteContainer.appendChild(streamDiv)
 }
 
-document.getElementById("join").onclick = function () { //join a call
+document.getElementById("join").onclick = function () {
     let channelName = document.getElementById("channelName").value;
     let Username = document.getElementById("username").value;
 
-    client.join( //client w/ join function
+    client.join(
         null,
         channelName,
         Username,
         () => {
-            var localStream = AgoraRTC.createStream({ //create stream
-                video: true, //w/ video
-                audio: true, //and with audio
+            var localStream = AgoraRTC.createStream({
+                video: true,
+                audio: true,
             })
 
             localStream.init(function () {
                 localStream.play("SelfStream")
                 console.log(`App id: ${appId}\nChannel id: ${channelName}`)
-                client.publish(localStream) //publish stream
+                client.publish(localStream)
             })
 
             globalStream = localStream
         }
     )
 
-    client.on("stream-added", function (evt) { //notify when user has joined call
+    client.on("stream-added", function (evt) {
         console.log("Added Stream");
         client.subscribe(evt.stream, handlefail)
     })
 
-    client.on("stream-subscribed", function (evt) { //when user subsribes to stream
+    client.on("stream-subscribed", function (evt) {
         console.log("Subscribed Stream");
         let stream = evt.stream;
-        addVideoStream(stream.getId()); //for remote user
+        addVideoStream(stream.getId());
         stream.play(stream.getId());
     })
 
 
-    client.on("peer-leave", function (evt) { //one of peers has left the call
+    client.on("peer-leave", function (evt) {
         console.log("Peer has left")
         removeVideoStream(evt)
     }
     )
 }
+
 document.getElementById("video-mute").onclick = function () {
     if (!isVideoMuted) {
         globalStream.muteVideo();
